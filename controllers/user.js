abf3cbaaeb29771f0933d25405dc12f28e6d099e
Joshua Patterson <joshualpatterson@gmail.com>
2020-09-21 10:39:01 -0400

    Updated nodemailer settings.

--- controllers/user.js
+++ controllers/user.js
@@ -115,8 +115,8 @@ const reset = (req, res) => {
 
             let transporter = nodemailer.createTransport({
                 host: 'localhost',
-                port: 25,
-                secure: false,
+                port: 465,
+                secure: true,
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASSWORD
