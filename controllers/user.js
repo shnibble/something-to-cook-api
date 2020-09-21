c45dd0852778a769092e7bf7cb5cf56f10af8a0a
Joshua Patterson <joshualpatterson@gmail.com>
2020-09-21 10:45:39 -0400

    Additional postfix testing.

--- controllers/user.js
+++ controllers/user.js
@@ -115,8 +115,8 @@ const reset = (req, res) => {
 
             let transporter = nodemailer.createTransport({
                 host: 'localhost',
-                port: 465,
-                secure: true,
+                port: 587,
+                secure: false,
                 auth: {
                     user: process.env.EMAIL_USER,
                     pass: process.env.EMAIL_PASSWORD
